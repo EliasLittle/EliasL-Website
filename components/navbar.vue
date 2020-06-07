99dbd95bbd2308bc34180340ec396eac36da5a48
Elias Little <eliasjlittle@gmail.com>
2020-06-07 17:25:42 -0500

    Made navbar mobile friendly

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,21 +2,52 @@
   <v-app-bar tile >
     <v-toolbar-title>Elias</v-toolbar-title>
     <v-divider inset vertical class="ml-4 mr-2"></v-divider>
-    <v-btn text to="/" class="font-weight-light" nuxt>About Me</v-btn>
+    <v-toolbar-items>
+      <v-btn text to="/" class="font-weight-light" nuxt>About Me</v-btn>
+    </v-toolbar-items>
     
     <v-spacer></v-spacer>
 
-    <v-btn text to="/involvements" class="font-weight-light" nuxt>Involvements</v-btn>
-    <v-divider inset vertical class="mx-2"></v-divider>
-    <v-btn text to="/portfolio" class="font-weight-light" nuxt>Portfolio</v-btn>
-    <v-divider inset vertical class="mx-2"></v-divider>
-    <v-btn text to="/blog" class="font-weight-light" nuxt>Stochastic Theories</v-btn>
+    <div class="hidden-md-and-up">
+      <v-menu>
+        <template v-slot:activator="{ on }">
+          <v-icon v-on="on" large>mdi-menu</v-icon>
+        </template>
+        <v-list>
+          <v-list-item v-for="item in menu" :key="item.title">
+            <v-list-item-title><v-btn text :to="item.route" class="font-weight-light" nuxt>{{item.title}}</v-btn></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+
+    <v-toolbar-items class="hidden-sm-and-down" >
+        <v-btn text v-for="item in menu" :key="item.title" :to="item.route" class="font-weight-light" nuxt>
+          {{item.title}}
+        </v-btn>
+    </v-toolbar-items>
+
   </v-app-bar>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      menu: [
+        {
+          "title":"Involvements",
+          "route":"/involvements"
+        },
+        {
+          "title":"Portfolio",
+          "route":"/portfolio"
+        },
+        {
+          "title":"Stochastic Theories",
+          "route":"/blog"
+        }
+      ]
+    }
   },
   methods: {}
 }
