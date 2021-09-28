60a6f443540dec8acecbe44570b51e2a47c2f5d2
ejl242 <ejl242@cornell.edu>
2021-09-27 23:18:04 -0400

    updates + finally working (hopefully)

--- pages/test/_slug.vue
+++ pages/test/_slug.vue
@@ -1,9 +1,7 @@
 <template>
-  <p>Hello, World! I'm testing here...</p>
-  <p>{{ this.id }}</p>
-  <p>{{ this.lnk }}</p>
 </template>
 <script>
+import links from "~/content/links.json";
 //import links from '~/assets/json/links.json';
 //console.log(links["youtube"]);
 export default {
@@ -15,16 +13,16 @@ export default {
   },*/
 
   async asyncData({$content, params }) {
-      const lnks = await $content('links.json').fetch();
+      //const lnks = await $content('links.json').fetch();
       const id = params.slug; // When calling /abc the slug will be "abc
-      const lnk = lnks[id];
+      const lnk = links[id];
       //console.log("Slug: ", slug)
       //console.log("Link?: ", links[slug])
-      return { id, lnk };
+      return { id, link };
   },
   
-  /*mounted() {
-    window.location.href = this.links[this.slug]; 
-  },*/
+  mounted() {
+    window.location.href = this.link; 
+  },
 };
 </script>
